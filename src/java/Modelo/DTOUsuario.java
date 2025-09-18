66f531df90c839cb21945cad23ff50ddc9222760
Ouken <Ouken@192.168.1.46>
2025-09-17 23:55:19 -0500

    DTO, CRUD Y DAO USUARIO

---
+++ src/java/Modelo/DTOUsuario.java
@@ -0,0 +1,78 @@
+package Modelo;
+
+public class DTOUsuario {
+    private int idUsuario;
+    private String appat;
+    private String apmat;
+    private String nombre;
+    private int dni;
+    private String email;
+    private String contra;
+    private int estado;
+
+    public DTOUsuario() {}
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getAppat() {
+        return appat;
+    }
+
+    public void setAppat(String appat) {
+        this.appat = appat;
+    }
+
+    public String getApmat() {
+        return apmat;
+    }
+
+    public void setApmat(String apmat) {
+        this.apmat = apmat;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContra() {
+        return contra;
+    }
+
+    public void setContra(String contra) {
+        this.contra = contra;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+}

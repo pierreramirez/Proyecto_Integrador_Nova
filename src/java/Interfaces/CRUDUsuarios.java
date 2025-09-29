de0e19fde4e9569464c0ee8fb91171133796b7b0
Ouken <Ouken@192.168.1.46>
2025-09-29 01:24:16 -0500

    UWU

--- src/java/Interfaces/CRUDUsuarios.java
+++
@@ -1,9 +0,0 @@
-package Interfaces;
-
-import Modelo.DTOUsuario;
-import java.util.LinkedList;
-
-public interface CRUDUsuarios {
-    public LinkedList<DTOUsuario> ListarUsuarios();
-    DTOUsuario ValidarSesion(String correo, String contra);
-}
